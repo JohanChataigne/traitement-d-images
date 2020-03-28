b469cf3e071fa3d7899f958ea0ea9375212559bb
Paul Germon <pgermon@enseirb-matmeca.fr>
2020-03-28 15:39:24 +0100

    wave.c

--- im-proc/domain/wave.c
+++ im-proc/domain/wave.c
@@ -11,11 +11,38 @@ process(int tx, int ax, int ty, int ay, char* ims, char* imd){
     pnm image = pnm_load(ims);
     int cols = pnm_get_width(image);
     int rows = pnm_get_height(image);
-    pnm new_image = pnm_new(cols, rows, PnmRawPpm);
+    pnm new_image1 = pnm_new(cols, rows, PnmRawPpm);
+    pnm new_image2 = pnm_new(cols, rows, PnmRawPpm);
 
-    pnm_save(new_image, PnmRawPpm, imd);
+    double wx = 2*M_PI / tx;
+    double wy = 2*M_PI / ty;
+
+    for(int i = 0 ; i < rows ; i++) {
+        double sin_i = ax * sin(wx * i)
+        for (int j = 0 ; j < cols ; j++) {
+                for(int c = 0 ; c < 3 ; c++) {
+                  if(j + sin_i >= 0 && j + sin_i <= cols) {
+                      pnm_set_component(new_image1, i, j + sin_i, c, pnm_get_component(image, i, j, c));
+                  }
+                }
+        }
+    }
+
+    for(int j = 0 ; j < cols ; j++) {
+        double sin_j = ay * sin(wy * j)
+        for (int i = 0 ; i < rows ; i++) {
+                for(int c = 0 ; c < 3 ; c++) {
+                  if(i + sin_j >= 0 && i + sin_j <= cols) {
+                      pnm_set_component(new_image2, i + sin_j, i, c, pnm_get_component(new_image1, i, j, c));
+                  }
+                }
+        }
+    }
+
+    pnm_save(new_image2, PnmRawPpm, imd);
     pnm_free(image);
-    pnm_free(new_image);
+    pnm_free(new_image1);
+    pnm_free(new_image2);
 
 }
 
