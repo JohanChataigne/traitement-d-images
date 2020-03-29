dc2a09d0c29719968ebd43ec85fefa773751fcd4
Paul Germon <pgermon@enseirb-matmeca.fr>
2020-03-29 12:12:09 +0200

    sphere.c

--- im-proc/domain/sphere.c
+++ im-proc/domain/sphere.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 #define M_PI 3.14159265358979323846
-#define K 3
+#define K 1.0
 
 void
 process(char* ims, char* imd){
@@ -16,16 +16,21 @@ process(char* ims, char* imd){
     pnm new_image = pnm_new(cols, rows, PnmRawPpm);
 
     for(int i = 0 ; i < rows ; i++) {
-        double phi = i * 1.0 / rows * M_PI * 2;
         for (int j = 0 ; j < cols ; j++) {
-            double theta = j * 1.0 / cols * M_PI * 2;
-            double u = (1 - cos(phi)) * cos(theta);
-            double v = (1 - cos(phi)) * sin(theta);
-            int x = cols / 2 + (u + 1) * sqrt(pow(u, 2) + pow(v, 2));
-            int y = rows / 2 + (v + 1) * sqrt(pow(u, 2) + pow(v, 2));
-            for (int c = 0 ; c < 3 ; c++) {
-                    pnm_set_component(new_image, i, j, c, pnm_get_component(image, y, x, c));
-            }
+          int x = j - cols/2;
+          int y = i - rows/2;
+
+          double rmax = sqrt(pow(cols,2) + pow(rows,2))/2;
+          double r = sqrt(pow(x, 2) + pow(y, 2));
+          double theta = atan2(y, x);
+          double s = r + pow(r, 3 * (K / pow(rmax, 2)));
+
+          int u = s * cos(theta) + cols/2;
+          int v = s * sin(theta) + rows/2;
+
+          for (int c = 0 ; c < 3 ; c++) {
+            pnm_set_component(new_image, i, j, c, pnm_get_component(image, v, u, c));
+          }
         }
     }
 
