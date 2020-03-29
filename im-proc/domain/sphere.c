d8da1898024465d3c7b0e75dfb321f75520b6f22
Paul Germon <pgermon@enseirb-matmeca.fr>
2020-03-29 18:06:18 +0200

    sphere.c

--- im-proc/domain/sphere.c
+++ im-proc/domain/sphere.c
@@ -13,24 +13,33 @@ process(char* ims, char* imd){
     pnm image = pnm_load(ims);
     int cols = pnm_get_width(image);
     int rows = pnm_get_height(image);
+
+    int R = rows/2;
+
+    int new_R = R * K / 2 - (K - 2) * R;
+
+    //int new_cols = 2 * new_R;
+    //int new_rows = 2 * new_R;
+
     pnm new_image = pnm_new(cols, rows, PnmRawPpm);
-    double rmax = sqrt(pow(cols,2) + pow(rows,2))/2;
+
+    double rmax = sqrt(2) * R;
 
     for(int i = 0 ; i < rows ; i++) {
         for (int j = 0 ; j < cols ; j++) {
-          int x = j - cols/2;
-          int y = i - rows/2;
+          int x = j - R;
+          int y = i - R;
 
           double r = sqrt(pow(x, 2) + pow(y, 2));
           double theta = atan2(y, x);
           double s = r + pow(r, 3) * (K / pow(rmax, 2));
 
-          int u = s * cos(theta) + cols/2;
-          int v = s * sin(theta) + rows/2;
+          int u = s * cos(theta) + R;
+          int v = s * sin(theta) + R;
 
           for (int c = 0 ; c < 3 ; c++) {
             if(u >= 0 && u < cols && v >= 0 && v <  rows) {
-                if(pow(cols/3.5, 2) >= pow(x , 2) + pow(y , 2))
+                if(pow(new_R, 2) > pow(j - R, 2) + pow(i - R, 2))
                     pnm_set_component(new_image, i, j, c, pnm_get_component(image, v, u, c));
             }
           }
