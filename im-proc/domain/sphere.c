36af3f315bdf8883ac84d046dc4d466afcfc1594
JohanChataigne <jchataigner@enseirb-matmeca.fr>
2020-03-29 13:31:17 +0200

    last version of sphere

--- im-proc/domain/sphere.c
+++ im-proc/domain/sphere.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 #define M_PI 3.14159265358979323846
-#define K 1.0
+#define K 5.0
 
 void
 process(char* ims, char* imd){
@@ -14,22 +14,25 @@ process(char* ims, char* imd){
     int cols = pnm_get_width(image);
     int rows = pnm_get_height(image);
     pnm new_image = pnm_new(cols, rows, PnmRawPpm);
+    double rmax = sqrt(pow(cols,2) + pow(rows,2))/2;
 
     for(int i = 0 ; i < rows ; i++) {
         for (int j = 0 ; j < cols ; j++) {
           int x = j - cols/2;
           int y = i - rows/2;
 
-          double rmax = sqrt(pow(cols,2) + pow(rows,2))/2;
           double r = sqrt(pow(x, 2) + pow(y, 2));
           double theta = atan2(y, x);
-          double s = r + pow(r, 3 * (K / pow(rmax, 2)));
+          double s = r + pow(r, 3) * (K / pow(rmax, 2));
 
           int u = s * cos(theta) + cols/2;
           int v = s * sin(theta) + rows/2;
 
           for (int c = 0 ; c < 3 ; c++) {
-            pnm_set_component(new_image, i, j, c, pnm_get_component(image, v, u, c));
+            if(u >= 0 && u < cols && v >= 0 && v <  rows) {
+                if(pow(cols/3.5, 2) >= pow(x , 2) + pow(y , 2))
+                    pnm_set_component(new_image, i, j, c, pnm_get_component(image, v, u, c));
+            }
           }
         }
     }
